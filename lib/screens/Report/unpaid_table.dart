f2191facf34bfece8b5788511ac582b480072c39
Kaka krsn <kakakarsana50@gmail.com>
2022-06-17 14:52:23 +0700

    add for report page

---
+++ lib/screens/Report/unpaid_table.dart
@@ -0,0 +1,434 @@
+import 'package:flutter/material.dart';
+
+class UnpaidTable extends StatefulWidget {
+  const UnpaidTable({Key? key}) : super(key: key);
+
+  @override
+  State<UnpaidTable> createState() => _UnpaidTableState();
+}
+
+class _UnpaidTableState extends State<UnpaidTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Table(
+            border: TableBorder(
+                horizontalInside: BorderSide(
+                    width: 1, color: Colors.grey, style: BorderStyle.solid)),
+            children: [
+              TableRow(children: [
+                Text(''),
+                Text(
+                  'Clients',
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Invoices',
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Unpaid',
+                  textAlign: TextAlign.center,
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '2020',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Januari',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Februari',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Maret',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'April',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Mei',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Mei',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Juni',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Juli',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Agustus',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'September',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Oktober',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'November',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Desember',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '40',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    '80',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Rp. 4.000.000',
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
